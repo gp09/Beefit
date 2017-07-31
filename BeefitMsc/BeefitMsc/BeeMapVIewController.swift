0d5b33bc32dc8b0f40a5feb94637e17d25fcc260
Priyank <priyankscores@gmail.com>
2017-07-31 19:41:21 +0100

    Added graphics for login and sign up Also logic for both view

---
+++ BeefitMsc/BeefitMsc/BeeMapVIewController.swift
@@ -0,0 +1,35 @@
+//
+//  BeeMapVIewController.swift
+//  BeefitMsc
+//
+//  Created by Priyank on 31/07/2017.
+//  Copyright © 2017 priyank. All rights reserved.
+//
+
+import UIKit
+
+class BeeMapVIewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ BeefitMsc/BeefitMsc/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  BeefitMsc
+//
+//  Created by Priyank on 31/07/2017.
+//  Copyright © 2017 priyank. All rights reserved.
+//
+
+import UIKit
+
+class Constants: NSObject {
+
+}

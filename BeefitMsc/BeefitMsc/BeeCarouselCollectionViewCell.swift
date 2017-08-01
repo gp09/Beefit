38f4aa0a7cf5f984734eb9e32082003b1a204a28
Priyank <priyankscores@gmail.com>
2017-08-01 18:20:08 +0100

    Trying to create custom collection cell for swiping cell

---
+++ BeefitMsc/BeefitMsc/BeeCarouselCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  BeeCarouselCollectionViewCell.swift
+//  BeefitMsc
+//
+//  Created by Priyank on 01/08/2017.
+//  Copyright © 2017 priyank. All rights reserved.
+//
+
+import UIKit
+
+class BeeCarouselCollectionViewCell: UICollectionViewCell {
+    
+}
